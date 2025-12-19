82dc80cd3b0a3ad5a3b387a6ce5c60c93285e4c9
Aman <amanaswani12345@gmail.com>
2025-12-19 19:19:37 +0530

    Empty main branch

--- backend/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { register, login } from "../controllers/auth.controller.js";
-import {getEvents} from "../controllers/event.controller.js";
-const router = express.Router();
-
-router.post("/register", register);
-router.post("/login", login);
-router.get("/events", getEvents);
-export default router;
